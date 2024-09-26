d83af7ae8a136374807e319bf3116cbbc5dfcd90
public-release <as-public-release@janestreet.com>
2024-09-26 19:08:47 +0100

    v0.18~preview.129.42+498

--- src/jsonafable.ml
+++ src/jsonafable.ml
@@ -30,7 +30,24 @@ module Stable = struct
       let jsonaf_of_t (t : M.t) : Jsonaf_kernel.t = `String (M.to_string t)
     end
   end
+
+  module Of_floatable = struct
+    module V1 (M : Floatable.S) : S with type t := M.t = struct
+      let t_of_jsonaf = function
+        | `Number s as json ->
+          (match M.of_float (Float.of_string s) with
+           | x -> x
+           | exception exn -> Jsonaf_kernel.Conv.of_jsonaf_error_exn exn json)
+        | json -> Jsonaf_kernel.Conv.of_jsonaf_error "number expected" json
+      ;;
+
+      let jsonaf_of_t (t : M.t) : Jsonaf_kernel.t =
+        `Number (!Sexplib0.Sexp_conv.default_string_of_float (M.to_float t))
+      ;;
+    end
+  end
 end
 
 module Of_jsonafable = Stable.Of_jsonafable.V1
 module Of_stringable = Stable.Of_stringable.V1
+module Of_floatable = Stable.Of_floatable.V1
--- src/jsonafable.mli
+++ src/jsonafable.mli
@@ -11,6 +11,7 @@ module Of_jsonafable
      end) : S with type t := M.t
 
 module Of_stringable (M : Stringable.S) : S with type t := M.t
+module Of_floatable (M : Floatable.S) : S with type t := M.t
 
 module Stable : sig
   module Of_jsonafable : sig
@@ -19,5 +20,9 @@ module Stable : sig
 
   module Of_stringable : sig
     module V1 : module type of Of_stringable
+  end
+
+  module Of_floatable : sig
+    module V1 : module type of Of_floatable
   end
 end
